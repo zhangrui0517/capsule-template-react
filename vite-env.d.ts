dde63afa581f33e84bff428e94324e038cda75f6
zhangrui <393394595@qq.com>
2023-02-25 10:55:40 +0800

    feat: add env files

--- vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
-  // 自定义的环境变量
-  readonly VITE_IMG_BASE_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,49 @@
-import { defineConfig /* , normalizePath */ } from 'vite'
+import { defineConfig } from 'vite'
 // import path from 'path'
 import react from '@vitejs/plugin-react'
 import postcssPresetEnv from 'postcss-preset-env'
 import viteEslint from 'vite-plugin-eslint'
 import viteStylelint from 'vite-plugin-stylelint'
 import svgr from 'vite-plugin-svgr'
+// import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 
-// const variablePath = normalizePath(path.resolve('./src/variable.scss'))
-
-export default defineConfig({
-  root: './src',
-  resolve: {
-    alias: {
-      '@assets': path.join(__dirname, 'src/assets')
-    }
-  },
-  plugins: [
-    react(),
-    viteEslint(),
-    viteStylelint({
-      exclude: ['node_modules']
-    }),
-    svgr()
-  ],
-  css: {
-    postcss: {
-      plugins: [postcssPresetEnv({ browsers: 'ie 9' })]
+export default defineConfig((/* config */) => {
+  return {
+    root: './src',
+    envDir: process.cwd(),
+    resolve: {
+      alias: {
+        '@assets': path.join(__dirname, 'src/assets')
+      }
     },
-    modules: {
-      generateScopedName: '[name]-[local]-[hash:base64:5]'
+    /** 依赖预构建相关 */
+    optimizeDeps: {
+      include: []
     },
-    preprocessorOptions: {
-      scss: {
-        // additionalData: `@import "${variablePath}";`
+    plugins: [
+      react(),
+      viteEslint({
+        exclude: ['node_modules']
+      }),
+      viteStylelint({
+        exclude: ['node_modules']
+      }),
+      /** 将svg文件直接转换为react组件使用 */
+      svgr()
+    ],
+    css: {
+      postcss: {
+        /** 自动补全css前缀 */
+        plugins: [postcssPresetEnv({ browsers: 'ie 9' })]
+      },
+      modules: {
+        generateScopedName: '[name]-[local]-[hash:base64:5]'
+      },
+      preprocessorOptions: {
+        scss: {
+          // additionalData: `@import "${variablePath}";`
+        }
       }
     }
   }
